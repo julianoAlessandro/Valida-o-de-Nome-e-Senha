553457292430dc7eba1b54f01aa7c8b9594a6ced
Juliano Alessandro dos Santos <111141842+julianoAlessandro@users.noreply.github.com>
2022-10-20 15:43:10 -0300

    Add files via upload

---
+++ Exercicio Sistema de validação.py
@@ -0,0 +1,18 @@
+nome = input("Informe seu nome :")
+senha = int(input("Informe sua senha:"))
+
+while nome  !=  "Juliano":
+    print("Nome  invalido!")
+    nome = input("Informe Novamente seu nome:")
+print("Nome validado")
+
+while  senha != 123:
+    print("Senha  invalida!")
+    senha = int(input("Informe Novamente sua senha:"))
+print("Senha validada!")
+
+
+print("Dados validados com sucesso")
+    
+    
+    
